12eb92b689b9d94d0d39baebfd1e9ccfb6782dbd
Licacio <tana.jan74@gmail.com>
2022-08-19 02:20:41 +0700

    deploy test

--- server/server.js
+++ server/server.js
@@ -5,9 +5,11 @@ const cors = require('cors')
 require('dotenv').config();
 const {readdirSync }  = require('fs')
 const connectDB = require("./config/db")
+const path = require("path")
 
 
 const app = express();
+
  
 connectDB();
 //middleware
@@ -15,11 +17,15 @@ connectDB();
 app.use(morgan("dev"))
 app.use(bodyParser.json({limit: "2mb"}))
 app.use(cors())
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 //Auto Route
 readdirSync('./routes')
     .map(r => app.use("/api",require("./routes/" + r )))
 
 const port = process.env.PORT || 8000;
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(port,()=>console.log('Server is runnning on port'  + port))
 
